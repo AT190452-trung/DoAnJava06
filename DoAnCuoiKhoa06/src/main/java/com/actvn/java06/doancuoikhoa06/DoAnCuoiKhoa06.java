592b6fdee3e801d715e7ab0d710e0d43c56f8255
AT190452-AnhTrung <anhtrungn560@gmail.com>
2023-08-06 21:50:16 +0700

    start

---
+++ DoAnCuoiKhoa06/src/main/java/com/actvn/java06/doancuoikhoa06/DoAnCuoiKhoa06.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.actvn.java06.doancuoikhoa06;
+
+/**
+ *
+ * @author HP
+ */
+public class DoAnCuoiKhoa06 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
